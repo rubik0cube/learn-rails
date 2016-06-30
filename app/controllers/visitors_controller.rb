c543e7e8e8915a3d9876006ba0218839a088a397
rubik0cube <huangfu2@illinois.edu>
2016-06-30 16:39:25 -0600

    dynamic home page

---
+++ app/controllers/visitors_controller.rb
@@ -0,0 +1,10 @@
+class VisitorsController < ApplicationController
+
+	#Define the new method. Create an instance variable
+	#named @owner and assign an instance of the Owner model
+	def new
+		@owner = Owner.new
+		render 'visitors/new'
+	end
+
+end
