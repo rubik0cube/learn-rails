0b99e36c95e04dfcae930fde9e7eed3a99307d6f
rubik0cube <huangfu2@illinois.edu>
2016-07-05 20:57:13 -0600

    miscellaneous for learning ruby

---
+++ lib/example.rb
@@ -0,0 +1,74 @@
+class Example < Object
+
+	# This is a comment
+
+	attr_accessor :honorific
+	attr_accessor :name
+	attr_accessor :date
+
+	def initialize(myname, date)
+
+		#assign instance variables
+		@name = myname
+		@date = date.nil? ? Date.today : date
+	end
+
+	def backwards_name
+		@name.reverse
+	end
+
+	def to_s
+		#overriding
+		@name
+	end
+
+	def titled_name
+
+		#if honorific is nil, false or undefined, assign it to 'Esteemed'
+		@honorific ||='Esteemed'
+
+		#interpolation, combine honorific and name into one string
+		titled_name = "#{@honorific} #{@name}"
+	end
+
+	def december_birthdays
+		born_in_december = []
+		famous_birthdays.each do |name, date|
+			if date.month == 12
+				born_in_december << name
+			end
+		end
+		born_in_december
+	end
+
+	private
+
+	def famous_birthdays
+
+		#Hash operator, associate name with date
+		birthdays = {
+			'Ludwig van Beethoven' => Date.new(1770,12,16),
+      		'Dave Brubeck' => Date.new(1920,12,6),
+      		'Buddy Holly' => Date.new(1936,9,7),
+      		'Keith Richards' => Date.new(1943,12,18)
+		}
+	end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
