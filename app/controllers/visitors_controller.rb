e866f128a34e443d414fccdec79e8edf08605853
rubik0cube <huangfu2@illinois.edu>
2016-07-06 19:07:09 -0600

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,7 +4,11 @@ class VisitorsController < ApplicationController
 	#named @owner and assign an instance of the Owner model
 	def new
 		@owner = Owner.new
-		render 'visitors/new'
+		
+		flash.now[:notice] = 'Welcome!'
+		flash.now[:alert] = 'My birthday is soon'
+
+		#render 'visitors/new'
 	end
 
 
