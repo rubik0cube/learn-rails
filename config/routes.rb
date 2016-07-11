e043e5ccb50f3003e32885de0c5aecb7de778eda
rubik0cube <huangfu2@illinois.edu>
2016-07-10 18:03:57 -0600

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   #Any request to the applocation root will be directed to
   #the VisitorsController new action
   
+
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new' 
 
 end
